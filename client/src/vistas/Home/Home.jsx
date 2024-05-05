0d319b5ece2ecca8b42c155c80519da5c7334c06
Ariel G Rogel <95644790+Aricoins@users.noreply.github.com>
2024-05-04 22:29:29 -0300

    Update Home.jsx

--- client/src/vistas/Home/Home.jsx
+++ client/src/vistas/Home/Home.jsx
@@ -6,20 +6,20 @@ import Nav from '../../Components/Nav';
 import SearchBar from '../../Components/SearchBar';
 import styled from 'styled-components';
 import imagen from "../Landing/icono.png";
-import colores from "../../vistas/colores"
-import { setPage, setTotalPages } from '../../redux/actions/paginationActions';
-import gif from "../../assets/gif3.gif"
+import colores from "../../vistas/colores";
+import gif from "../../assets/gif3.gif";
+
 const H2 = styled.h2`
-  display:flex;
+  display: flex;
   flex-direction: row;
   justify-content: center;
   color: ${colores.verde};
-  font-size:40px;
+  font-size: 40px;
   padding: 0%;
   width: 40%;
-  margin-left:0%;
-  @media only screen and (max-width : 700px) {
-    font-size: 20px; 
+  margin-left: 0%;
+  @media only screen and (max-width: 700px) {
+    font-size: 20px;
     width: 20%;
     margin-left: 0%;
     height: 20%;
@@ -34,61 +34,63 @@ const Barra = styled.div`
   height: 8rem;
   align-items: right;
 `;
-/*
-const spinAnimation = keyframes`
-  to {
-    transform: rotate(360deg);
-  }
-`;
-const LoadingIndicator = styled.div`
- display: inline-block;
-  width: 40px;
-  height: 40px;
-  border: 4px solid rgba(255, 255, 255, 0.3);
-  border-radius: 50%;
-  border-top: 4px solid #ffffff;
-  background-color: ${colores.amarillo}
-  animation: ${spinAnimation} 1s ease-in-out infinite;
-`; */
 
 const Home = () => {
   const dispatch = useDispatch();
-  const dogs = useSelector((state) => state.dogs);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize, setPageSize] = useState(8); // Ajusta el tamaño de la página según sea necesario
+
+  // Guarda los datos paginados, el número total de perros y el estado de carga
+  const [paginatedDogs, setPaginatedDogs] = useState([]);
+  const [totalPages, setTotalPages] = useState(0);
   const [loading, setLoading] = useState(true);
 
+  // Solicita datos de la página actual
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchDogs = async () => {
       try {
-        await dispatch(getDogs());
+        const response = await dispatch(getDogs(currentPage, pageSize));
+        setPaginatedDogs(response.dogs);
+        setTotalPages(response.totalPages);
         setLoading(false);
       } catch (error) {
-        // Handle error if needed
+        console.error('Error fetching dogs:', error);
         setLoading(false);
       }
     };
+    fetchDogs();
+  }, [dispatch, currentPage, pageSize]);
 
-    fetchData();
-  }, [dispatch]);
+  // Maneja el cambio de página
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
 
   return (
     <>
       <Barra>
-        <H2><img style={{width:"30%", height:"140%"}} src={imagen} alt="hueso"/> Dogs App</H2>
+        <H2><img style={{ width: "30%", height: "140%" }} src={imagen} alt="hueso" /> Dogs App</H2>
         <SearchBar />
       </Barra>
 
       <Nav />
 
-     {loading ? (  <>
-      <div style={{display: "flex", flexDirection: "row", justifyContent: "center"}}>
-      <img style={{padding:"1%",width:"25%", borderRadius:"50%"}} src={gif} alt="hueso"/>  
-      </div>
-      <span style={{padding:"1%", display: "flex", flexDirection: "row", justifyContent: "center", color: "white"}}>Loading...</span>
-      </>)
-     : (
-        <Cards dogs={dogs} />
-      )} 
-    </>  
+      {loading ? (
+        <>
+          <div style={{ display: "flex", flexDirection: "row", justifyContent: "center" }}>
+            <img style={{ padding: "1%", width: "25%", borderRadius: "50%" }} src={gif} alt="cargando" />
+          </div>
+          <span style={{ padding: "1%", display: "flex", flexDirection: "row", justifyContent: "center", color: "white" }}>Loading...</span>
+        </>
+      ) : (
+        <Cards
+          paginatedDogs={paginatedDogs}
+          totalPages={totalPages}
+          currentPage={currentPage}
+          onPageChange={handlePageChange}
+        />
+      )}
+    </>
   );
 };
 
