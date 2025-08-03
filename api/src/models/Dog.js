04b6d55eba7f382cb63a957dffc15793b31a4c57
Ariel Rogel <95644790+Aricoins@users.noreply.github.com>
2025-08-03 19:33:00 -0300

    Update Dog.js

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -2,46 +2,45 @@ const { DataTypes } = require('sequelize');
 const { v4: uuidv4 } = require('uuid');
 
 module.exports = (sequelize) => {
-  // Modelo para Dogs
-    const Dog = sequelize.define('Dog', {
-      id: {
-              type: DataTypes.UUID,
-              defaultValue: DataTypes.UUIDV4,
-              allowNull: false,
-              primaryKey: true,
-          },
-
+  const Dog = sequelize.define('Dog', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
     nombre: {
-                type: DataTypes.STRING,
-                allowNull: true,
-                validate: {
-                len: {
-                args: [2, 20],
-                msg: "El nombre debe tener entre 2 y 20 caracteres",
-                },
+      type: DataTypes.STRING,
+      allowNull: false, // Cambiado a false
+      validate: {
+        len: {
+          args: [2, 20],
+          msg: "El nombre debe tener entre 2 y 20 caracteres",
+        },
       },
     },
     imagen: {
       type: DataTypes.STRING,
-      allowNull: true,
-      defaultValue: 'https://cdn2.thedogapi.com/images/BJa4kxc4X.jpg',
+      allowNull: false, // Cambiado a false
+      validate: {
+        isUrl: true // Validación adicional
+      }
     },
     altura: {
       type: DataTypes.FLOAT,
-      allowNull: true,
+      allowNull: false, // Cambiado a false
     },
     peso: {
       type: DataTypes.FLOAT,
-      allowNull: true,
+      allowNull: false, // Cambiado a false
     },
     anios: {
       type: DataTypes.INTEGER,
-      allowNull: true,
-    },
-    temperament: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
+      allowNull: false, // Cambiado a false
+      validate: {
+        min: 1 // Validación adicional
+      }
+    }
   });
 
   return Dog;
