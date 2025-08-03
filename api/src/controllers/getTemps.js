85826ed13b4c6fae1de32b6acc715afab66c26b7
aricoinss <arielrogeldev@gmail.com>
2025-08-03 20:15:22 -0300

    Fix temperaments API by adding missing API key to TheDogAPI request

--- api/src/controllers/getTemps.js
+++ api/src/controllers/getTemps.js
@@ -1,9 +1,12 @@
 const axios = require("axios");
 const { Temperament } = require("../db");
 
+require("dotenv").config();
+const { API_KEY } = process.env;
+
 async function getTemps() {
   try {
-    const response = await axios.get(`https://api.thedogapi.com/v1/breeds`);
+    const response = await axios.get(`https://api.thedogapi.com/v1/breeds?api_key=${API_KEY}`);
     const dogs = response.data;
 
     // Paso 1: Extraer y normalizar todos los temperamentos únicos
